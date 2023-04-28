733480a16bb87a25c243359af4529a53adc350a7
spkl <spkl>
2023-04-28 16:52:36 +0200

    add MessageChannelHost.Shutdown

--- spkl.IPC/Messaging/MessageChannelHost.cs
+++ spkl.IPC/Messaging/MessageChannelHost.cs
@@ -29,6 +29,11 @@ namespace spkl.IPC.Messaging
             this.listenerThread.Start();
         }
 
+        public void Shutdown()
+        {
+            this.Socket.Close();
+        }
+
         private void Accept()
         {
             while (true)
