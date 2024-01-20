e388d32a7e1d96dda20c48c62eb226b66697945d
spkl <spkl>
2024-01-20 15:35:26 +0100

    add generic StartupBehavior class

---
+++ src/Startup/StartupBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace spkl.IPC.Startup;
+public class StartupBehavior : IStartupBehavior
+{
+    private readonly Action startInstanceAction;
+
+    public StartupBehavior(string negotiationFileBasePath, TimeSpan pollingPeriod, TimeSpan timeoutThreshold, Action startInstanceAction)
+    {
+        this.NegotiationFileBasePath = negotiationFileBasePath;
+        this.PollingPeriod = pollingPeriod;
+        this.TimeoutThreshold = timeoutThreshold;
+        this.startInstanceAction = startInstanceAction;
+    }
+
+    public string NegotiationFileBasePath { get; }
+
+    public TimeSpan PollingPeriod { get; }
+
+    public TimeSpan TimeoutThreshold { get; }
+
+    public void StartInstance()
+    {
+        this.startInstanceAction();
+    }
+}
