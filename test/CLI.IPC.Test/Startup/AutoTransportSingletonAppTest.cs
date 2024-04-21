cd1737bd03ee0eb809b7e58efea57a106dd150eb
spkl <seba.fischer@gmail.com>
2024-04-21 22:02:14 +0200

    added more granular tests for AutoTransportSingletonApp

--- test/CLI.IPC.Test/Startup/AutoTransportSingletonAppTest.cs
+++ test/CLI.IPC.Test/Startup/AutoTransportSingletonAppTest.cs
@@ -1,7 +1,9 @@
-﻿using spkl.CLI.IPC.Startup;
+﻿using spkl.CLI.IPC.Internal;
+using spkl.CLI.IPC.Startup;
 using System.IO;
 
 namespace spkl.CLI.IPC.Test.Startup;
+
 internal class AutoTransportSingletonAppTest : SingletonAppTestBase
 {
     protected AutoTransportSingletonApp singletonApp = null!;
@@ -16,6 +18,105 @@ internal class AutoTransportSingletonAppTest : SingletonAppTestBase
     public void TearDown()
     {
         this.singletonApp.Dispose();
+    }
+
+    [Test]
+    public void RequestInstanceReturnsWithDeserializedTransportIfApplicationRunning()
+    {
+        // arrange
+        this.disposables.Add(File.Open(this.negotiationFile + ".run_lock", FileMode.Create));
+        this.disposables.Add(File.Open(this.negotiationFile + ".transport_ready", FileMode.Create));
+        using (FileStream type = File.Open(this.negotiationFile + ".transport_type", FileMode.Create))
+        using (FileStream data = File.Open(this.negotiationFile + ".transport_data", FileMode.Create))
+        {
+            Serializer.Write(new TcpLoopbackTransport(1234), type, data);
+        }
+
+        // act
+        this.singletonApp.RequestInstance();
+        ITransport transport = this.singletonApp.Transport;
+
+        // assert
+        Assert.That(transport, Is.InstanceOf<TcpLoopbackTransport>());
+        Assert.That(((TcpLoopbackTransport)transport).Port, Is.EqualTo(1234));
+    }
+
+    [Test]
+    public void RequestInstanceThrowsExceptionIfTransportDataIsNotReady()
+    {
+        // arrange
+        this.disposables.Add(File.Open(this.negotiationFile + ".run_lock", FileMode.Create));
+
+        // act & assert
+        Assert.That(() => this.singletonApp.RequestInstance(), Throws.InstanceOf<SingletonAppException>());
+    }
+
+    [Test]
+    public void RequestInstanceThrowsExceptionIfTransportDataIsReadyButMissing()
+    {
+        // arrange
+        this.disposables.Add(File.Open(this.negotiationFile + ".run_lock", FileMode.Create));
+        this.disposables.Add(File.Open(this.negotiationFile + ".transport_ready", FileMode.Create));
+
+        // act & assert
+        Assert.That(() => this.singletonApp.RequestInstance(), Throws.InstanceOf<SingletonAppException>());
+    }
+
+    [Test]
+    public void ReportInstanceRunningLocksFile()
+    {
+        // act
+        this.singletonApp.ReportInstanceRunning();
+
+        // assert
+        Assert.That(
+            () => this.disposables.Add(File.Open(this.negotiationFile + ".transport_ready", FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite | FileShare.Delete)),
+            Throws.InstanceOf<IOException>());
+    }
+
+    [Test]
+    public void ReportInstanceRunningSerializesTransport()
+    {
+        // arrange
+        this.singletonApp.Transport = new TcpLoopbackTransport(2345);
+
+        // act
+        this.singletonApp.ReportInstanceRunning();
+
+        // assert
+        using (FileStream type = File.Open(this.negotiationFile + ".transport_type", FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete))
+        using (FileStream data = File.Open(this.negotiationFile + ".transport_data", FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete))
+        {
+            ITransport transport = Serializer.Read<ITransport>(type, data);
+
+            Assert.That(transport, Is.InstanceOf<TcpLoopbackTransport>());
+            Assert.That(((TcpLoopbackTransport)transport).Port, Is.EqualTo(2345));
+        }
+    }
+
+    [Test]
+    public void ReportInstanceRunningThrowsExceptionIfFileIsLocked()
+    {
+        // arrange
+        this.disposables.Add(File.Open(this.negotiationFile + ".transport_ready", FileMode.Create, FileAccess.ReadWrite, FileShare.None));
+
+        // act & assert
+        Assert.That(() => this.singletonApp.ReportInstanceRunning(), Throws.InstanceOf<SingletonAppException>());
+    }
+
+    [Test]
+    public void ShutdownInstanceUnlocksFiles()
+    {
+        // arrange
+        this.singletonApp.ReportInstanceRunning();
+
+        // act
+        this.singletonApp.ShutdownInstance();
+
+        // assert
+        Assert.That(() => this.disposables.Add(File.Open(this.negotiationFile + ".transport_ready", FileMode.Create)), Throws.Nothing);
+        Assert.That(() => this.disposables.Add(File.Open(this.negotiationFile + ".transport_type", FileMode.Create)), Throws.Nothing);
+        Assert.That(() => this.disposables.Add(File.Open(this.negotiationFile + ".transport_data", FileMode.Create)), Throws.Nothing);
     }
 
 #if NET6_0_OR_GREATER
