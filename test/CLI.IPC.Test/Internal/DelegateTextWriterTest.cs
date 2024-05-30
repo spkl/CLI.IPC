65cf26fff0af7b6cbbe54c7c2ffc4f07713f7d77
spkl <seba.fischer@gmail.com>
2024-05-30 15:52:53 +0200

    Added more test coverage for DelegateTextWriter

--- test/CLI.IPC.Test/Internal/DelegateTextWriterTest.cs
+++ test/CLI.IPC.Test/Internal/DelegateTextWriterTest.cs
@@ -1,19 +1,74 @@
 ﻿using spkl.CLI.IPC.Internal;
+using System.Collections.Generic;
 using System.Text;
 
 namespace spkl.CLI.IPC.Test.Internal;
 internal class DelegateTextWriterTest : TestBase
 {
+    private readonly List<string> writtenStrings = new();
+
+    private DelegateTextWriter writer = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        this.writtenStrings.Clear();
+        this.writer = new DelegateTextWriter(this.writtenStrings.Add);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        this.writer.Dispose();
+    }
+
     [Test]
     public void EncodingIsUtf8()
     {
-        // arrange
-        DelegateTextWriter writer = new(s => { });
-
         // act
-        Encoding result = writer.Encoding;
+        Encoding result = this.writer.Encoding;
 
         // assert
         Assert.That(result, Is.EqualTo(Encoding.UTF8));
+    }
+
+    [Test]
+    public void WriteChar()
+    {
+        // act
+        this.writer.Write('x');
+
+        // assert
+        Assert.That(this.writtenStrings, Is.EqualTo(new string[] { "x" }));
+    }
+
+    [Test]
+    public void WriteCharArrayIntInt()
+    {
+        // act
+        this.writer.Write("foobar".ToCharArray(), 1, 4);
+
+        // assert
+        Assert.That(this.writtenStrings, Is.EqualTo(new string[] { "ooba" }));
+    }
+
+    [Test]
+    public void WriteString()
+    {
+        // act
+        this.writer.Write("foobar");
+
+        // assert
+        Assert.That(this.writtenStrings, Is.EqualTo(new string[] { "foobar" }));
+    }
+
+    [Test]
+    public void WriteStringNull()
+    {
+        // act
+        this.writer.Write((string?)null);
+
+        // assert
+        Assert.That(this.writtenStrings, Is.Empty);
     }
 }
